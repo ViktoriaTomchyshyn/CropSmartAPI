86bae26dbb38b58647bcbdd3e25c33183ef3d70b
ViktoriaTomchyshyn <viktoriia.tomchyshyn@gmail.com>
2024-02-08 18:09:30 +0300

    ml model added

---
+++ CropSmartAPI/CropSmart.ML/Program.cs
@@ -0,0 +1,9 @@
+﻿// Load model and predict the next set values.
+// The number of values predicted is equal to the horizon specified while training.
+using CropSmart_ML;
+
+
+var result = CropPredictModel.Predict();
+
+Console.WriteLine(result.Fertility_LB[0] + ", " + result.Fertility_UB[0]);
+
