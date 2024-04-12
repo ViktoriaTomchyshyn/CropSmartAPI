50d8b6a4472d2fc3b96d8da185dde86917711267
ViktoriaTomchyshyn <viktoriia.tomchyshyn@gmail.com>
2024-04-12 12:36:17 +0300

    sqlite added

--- CropSmartAPI/CropSmartAPI.DAL/Context/DataContext.cs
+++ CropSmartAPI/CropSmartAPI.DAL/Context/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using CropSmartAPI.DAL.Entities;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -13,7 +14,26 @@ public class DataContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-JJTRH2D;Initial Catalog=CropSmartDB;Integrated Security=True");
+
+        if (!optionsBuilder.IsConfigured) // Ensure this is not already configured from elsewhere
+        {
+            // Check if SQL Server connection can be established
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-JJTRH2D;Initial Catalog=CropSmartDB;Integrated Security=True");
+                sqlConnection.Open();
+                sqlConnection.Close();
+
+                // If SQL Server connection is successful, use SQL Server
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-JJTRH2D;Initial Catalog=CropSmartDB;Integrated Security=True");
+            }
+            catch (Exception)
+            {
+                // If SQL Server connection fails, use SQLite
+                optionsBuilder.UseSqlite("Data Source=cropsmartDB.db");
+            }
+        }
+        //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-JJTRH2D;Initial Catalog=CropSmartDB;Integrated Security=True");
     }
     public async Task CompleteAsync()
     {
