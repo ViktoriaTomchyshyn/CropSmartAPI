dd0604d7224ee75efe5b86dbf7066c932b0a70ff
svyatopato <svyatopato@gmail.com>
2024-04-27 22:30:11 +0300

    Remove "Not found" for get all fields endpoint

--- CropSmartAPI/CropSmartAPI.Server/Commands/Field/GetFieldsByUserQuery.cs
+++ CropSmartAPI/CropSmartAPI.Server/Commands/Field/GetFieldsByUserQuery.cs
@@ -31,11 +31,6 @@ public class GetFieldsByUserQuery : IRequest<Result<List<FieldDto>, string>>
             var userId = int.Parse(item);
             var field = await _fieldService.Fields(userId, request.SearchQuery);
 
-            if (!field?.Any() ?? true)
-            {
-                return Result.Failure<List<FieldDto>, string>("Field not found");
-            }
-
             var result = field.Select(p => new FieldDto
             {
                 Id = p.Id,
